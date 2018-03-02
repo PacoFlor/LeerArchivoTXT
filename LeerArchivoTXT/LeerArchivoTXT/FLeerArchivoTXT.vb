d23247f6d4775ec41f166c2895f3aff5c670497b
PacoFlor <pacoflor@outlook.es>
2018-03-02 15:48:00 +0100

    primera version LeearArchivoTXT

---
+++ LeerArchivoTXT/LeerArchivoTXT/FLeerArchivoTXT.vb
@@ -0,0 +1,80 @@
+﻿Imports System.IO
+
+
+Public Class FLeerArchivoTXT
+
+    Private Sub BTPulsar_Click(sender As Object, e As EventArgs) Handles BTPulsar.Click
+
+        ' utiliza el que quieras prefiero el listbox
+
+        'listbox()
+
+        textbox()
+
+    End Sub
+
+    Public Sub textbox()
+
+
+        If OpenFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+
+            Dim sr As New System.IO.StreamReader(OpenFileDialog1.FileName)
+
+            Dim Contenido As String = ""
+            Dim intermedio As String = ""
+
+            Try
+
+                While sr.Peek() <> -1
+
+                    Contenido = sr.ReadLine()
+
+                    intermedio = intermedio + Contenido + Chr(13)
+
+                End While
+
+                TXTArchivotexto.Text = intermedio
+
+            Catch ex As Exception
+
+                MessageBox.Show(ex.ToString())
+
+            End Try
+
+            sr.Close()
+
+        End If
+
+    End Sub
+
+    Public Sub listbox()
+
+        If OpenFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+
+            Dim sr As New System.IO.StreamReader(OpenFileDialog1.FileName)
+
+            Dim Contenido As String = ""
+
+            Try
+
+                While sr.Peek() <> -1
+
+                    Contenido = sr.ReadLine()
+
+                    LArchivotexto.Items.Add(Contenido)
+
+                End While
+
+            Catch ex As Exception
+
+                MessageBox.Show(ex.ToString())
+
+            End Try
+
+            sr.Close()
+
+        End If
+
+    End Sub
+
+End Class
